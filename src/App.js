e49a56456cc808f39b83ceedc13b343c7358f9a9
Dhrutik-Patel <dhrutikpatel2017@gmail.com>
2023-07-22 11:19:52 +0530

    react router dom with shared components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SharedComponent from './components/SharedComponent';
+import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
+import ProductPage from './Pages/ProductPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SharedComponent />}>
+          <Route index element={<HomePage />} />
+          <Route path='about' element={<AboutPage />} />
+          <Route path='products' element={<ProductPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/SharedComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import { Outlet } from 'react-router-dom';
+
+const SharedComponent = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+export default SharedComponent;
